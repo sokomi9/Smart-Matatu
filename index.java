cb3c3290dedb5b248d54f88f3e2b3678b56ea16e
Abiud Sokomi <abiudsokomi@gmail.com>
2024-02-14 22:07:34 +0300

    my commit

---
+++ index.java
@@ -0,0 +1,17 @@
+class Route
+{
+    public static void main(String a [])
+    {
+        int Mirema = 18000;
+        int Kayole = 16000;
+        int Kasarani = 120000;
+
+        if(Mirema > Kayole && Mirema > Kasarani)
+        System.out.println("Mirema");
+        else if(Kayole > Kasarani)
+        System.out.println("Kayole");
+        else
+        System.out.println("Kasarani");
+    }
+}
+
